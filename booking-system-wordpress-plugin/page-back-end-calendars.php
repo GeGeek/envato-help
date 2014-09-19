00209245cb557577f39ea08a19278d2a60f9e5ef
Marius <marius.donea@dotonpaper.net>
2014-09-19 13:11:52 +0300

    v1.0 - b3.1

--- booking-system-wordpress-plugin/page-back-end-calendars.php
+++ booking-system-wordpress-plugin/page-back-end-calendars.php
@@ -6,12 +6,19 @@
                     <p>
                         Here you can create calendars and add availability, price, status, information for days and hours.
                     </p>
+                </section>
+                
+                <section>
+                    <h4>Important information</h4>
+                    
                     <p>
-                        <strong><em>IMPORTANT!</em> Availability is not set by default. Because of the big number of possibilities that exist, we cannot set a default for all business models.</strong>
+                        Availability is not set by default. Because of the big number of possibilities that exist, we cannot set a default for all business models. Follow the below instructions on how to create a calendar and set availability to it.
                     </p>
                 </section>
                 
                 <section>
+                    <h2>Set availability</h2>
+                    
                     <!--
                         Toggle buttons
                     -->
@@ -32,10 +39,10 @@
                         <a class="toggle" href="javascript:void(0)">Add a calendar <span class="icon"></span></a>
                         <div class="toggle-content">
                             <ul>
-                                <li><strong class="bold">Step 1</strong> - Click the <strong class="bold">"plus"</strong> button to add a new calendar.</li>
+                                <li><strong>Step 1</strong> - Click the <strong class="bold">plus</strong> button to add a new calendar.</li>
                             </ul>
                             <p>
-                                <img src="http://envato-help.dotonpaper.net/booking-system-wordpress-plugin/content/back-end-calendars-add.jpg" alt="" />
+                                <img src="http://envato-help.dotonpaper.net/booking-system-wordpress-plugin/content/back-end-calendars-add.jpg" alt="Booking System WordPress Plugin - Add calendar in back end" title="Booking System WordPress Plugin - Add calendar in back end" />
                             </p>
                             <p>
                                 After you clicked the button, the calendar will appear in the list below.
@@ -53,12 +60,12 @@
                                 <li><strong class="bold">Step 1</strong> - Select the calendar from the list to display the information.</li>
                             </ul>
                             <p>
-                                <img src="http://envato-help.dotonpaper.net/booking-system-wordpress-plugin/content/back-end-calendars-set.jpg" alt="" />
+                                <img src="http://envato-help.dotonpaper.net/booking-system-wordpress-plugin/content/back-end-calendars-set.jpg" alt="Booking System WordPress Plugin - Set calendar days availability in back end" title="Booking System WordPress Plugin - Set calendar days availability in back end" />
                             </p>
                             <ul>
-                                <li><strong class="bold">Step 2 & 3</strong> - To set availability you need to select first and last day you want to modify. To select one day click on it 2 times.</li>
-                                <li><strong class="bold">Step 4</strong> - Set availability, price, status ...</li>
-                                <li><strong class="bold">Step 5</strong> - Click <strong class="underline">Submit</strong> button to save the data you added.</li>
+                                <li><strong>Step 2 & 3</strong> - To set availability you need to select first and last day you want to modify. To select one day click on it 2 times.</li>
+                                <li><strong>Step 4</strong> - Set availability, price, status ...</li>
+                                <li><strong>Step 5</strong> - Click <strong class="bold">Submit</strong> button to save the data you added.</li>
                             </ul>
                         </div>
                     </div>
@@ -70,16 +77,16 @@
                         <a class="toggle" href="javascript:void(0)">Set availability for hours in multiple days <span class="icon"></span></a>
                         <div class="toggle-content">
                             <ul>
-                                <li><strong class="bold">Step 1 & 2</strong> - Select first and last day you want to modify. To select one day click on it 2 times.</li>
+                                <li><strong>Step 1 & 2</strong> - Select first and last day you want to modify. To select one day click on it 2 times.</li>
                             </ul>
                             <p>
-                                <img src="http://envato-help.dotonpaper.net/booking-system-wordpress-plugin/content/back-end-calendars-set-hours.jpg" alt="" />
+                                <img src="http://envato-help.dotonpaper.net/booking-system-wordpress-plugin/content/back-end-calendars-set-hours.jpg" alt="Booking System WordPress Plugin - Set calendar hours availability in multitple days in back end" title="Booking System WordPress Plugin - Set calendar hours availability in multitple days in back end" />
                             </p>
                             <ul>
-                                <li><strong class="bold">Step 3</strong> - Set days informations and notes. Availability, price & status will be calculated from hours data.</li>
-                                <li><strong class="bold">Step 4</strong> - Enter hours definitions, if you want them to be different than the default ones. Click the checkbox to save the data.</li>
-                                <li><strong class="bold">Step 5</strong> - Set availability, price, status ... for all hours from the selected days. Click the checkbox to save the data.</li>
-                                <li><strong class="bold">Step 6</strong> - Click <strong class="underline">Submit</strong> button to save the data you added.</li>
+                                <li><strong>Step 3</strong> - Set days informations and notes. Availability, price & status will be calculated from hours data.</li>
+                                <li><strong>Step 4</strong> - Enter hours definitions, if you want them to be different than the default ones. Click the checkbox to save the data.</li>
+                                <li><strong>Step 5</strong> - Set availability, price, status ... for all hours from the selected days. Click the checkbox to save the data.</li>
+                                <li><strong>Step 6</strong> - Click <strong class="bold">Submit</strong> button to save the data you added.</li>
                             </ul>
                         </div>
                     </div>
@@ -91,70 +98,66 @@
                         <a class="toggle" href="javascript:void(0)">Set availability for separate hours <span class="icon"></span></a>
                         <div class="toggle-content">
                             <p>
-                                <img src="http://envato-help.dotonpaper.net/booking-system-wordpress-plugin/content/back-end-calendars-set-hour.jpg" alt="" />
+                                <img src="http://envato-help.dotonpaper.net/booking-system-wordpress-plugin/content/back-end-calendars-set-hour.jpg" alt="Booking System WordPress Plugin - Set calendar hours availability in back end" title="Booking System WordPress Plugin - Set calendar hours availability in back end" />
                             </p>
                             <ul>
-                                <li><strong class="bold">Step 1</strong> - Click on the "clock" icon to display all hours for a day.</li>
-                                <li><strong class="bold">Step 2 & 3</strong> - Select first and last hours you want to modify. To select one hour click on it 2 times.</li>
-                                <li><strong class="bold">Step 4</strong> - Set availability, price, status ...</li>
-                                <li><strong class="bold">Step 5</strong> - Click <strong class="underline">Submit</strong> button to save the data you added.</li>
+                                <li><strong>Step 1</strong> - Click on the <strong class="bold">clock</strong> icon to display all hours for a day.</li>
+                                <li><strong>Step 2 & 3</strong> - Select first and last hours you want to modify. To select one hour click on it 2 times.</li>
+                                <li><strong>Step 4</strong> - Set availability, price, status ...</li>
+                                <li><strong>Step 5</strong> - Click <strong class="bold">Submit</strong> button to save the data you added.</li>
                             </ul>
                         </div>
                     </div>
+                </section>
+                
+                <section>
+                    <h2>Page details</h2>
                     
-                    <!--
-                        Details
-                    -->
-                    <div class="toggle-wrapper toggle-back-end-calendars displayed">
-                        <a class="toggle" href="javascript:void(0)">Details <span class="icon"></span></a>
-                        <div class="toggle-content">
-                            <ul>
-                                <li><strong class="bold">Button 1</strong> - Add calendar.</li>
-                                <li><strong class="bold">Button 2</strong> - Calendars help box.</li>
-                            </ul>
-                    
-                            <h6>Calendars list</h6>
-                            <ul>
-                                <li><strong class="bold">Item 3</strong> - Calendar ID.</li>
-                                <li><strong class="bold">Item 4</strong> - Calendar name.</li>
-                                <li><strong class="bold">Item 5</strong> - Calendar author.</li>
-                                <li><strong class="bold">Item 6</strong> - The number of pending reservations.</li>
-                                <li><strong class="bold">Item 7</strong> - The number of approved reservations.</li>
-                                <li><strong class="bold">Item 8</strong> - The number of rejected reservations.</li>
-                                <li><strong class="bold">Item 9</strong> - The number of canceled reservations.</li>
-                            </ul>  
-                    
-                            <p>
-                                <img src="http://envato-help.dotonpaper.net/booking-system-wordpress-plugin/content/back-end-calendars-description.jpg" alt="" />
-                            </p>
-                            
-                            <h6>Calendar buttons</h6>
-                            <ul>
-                                <li><strong class="bold">Button 10</strong> - Display calendar to set availability.</li>
-                                <li><strong class="bold">Button 11</strong> - Display calendar settings.</li>
-                                <li><strong class="bold">Button 12</strong> - Display calendar notifications settings.</li>
-                                <li><strong class="bold">Button 13</strong> - Display calendar payment gateways settings.</li>
-                                <li><strong class="bold">Button 14</strong> - Display calendar user permissions settings.</li>
-                                <li><strong class="bold">Button 15</strong> - Delete calendar. All data related to this calendar will be deleted, including reservations.</li>
-                                <li><strong class="bold">Button 16</strong> - Calendar help box.</li>
-                            </ul> 
-                            
-                            <h6>Availability form</h6>
-                            <ul>
-                                <li><strong class="bold">Button 17</strong> - Submit form data.</li>
-                                <li><strong class="bold">Button 18</strong> - Reset (remove) selected period data.</li>
-                                <li><strong class="bold">Button 19</strong> - Close form.</li>
-                                <li><strong class="bold">Item 20</strong> - Selected period first day.</li>
-                                <li><strong class="bold">Item 21</strong> - Selected period last day.</li>
-                                <li><strong class="bold">Item 22</strong> - Set selected days/hours status.</li>
-                                <li><strong class="bold">Item 23</strong> - Set selected days/hours price.</li>
-                                <li><strong class="bold">Item 24</strong> - Set selected days/hours promotional price.</li>
-                                <li><strong class="bold">Item 25</strong> - Set selected days/hours number of items available. Leave it blank to not display any number.</li>
-                                <li><strong class="bold">Item 26</strong> - Set selected days/hours information, which will be visible to the users.</li>
-                                <li><strong class="bold">Item 27</strong> - Set selected days/hours notes, which will be visible only to the administrators.</li>
-                                <li><strong class="bold">Item 28</strong> - Group selected days/hours into a single block. All data will be attributed to it.</li>
-                            </ul>
-                        </div>
-                    </div>
+                    <ul>
+                        <li><strong>Button 1</strong> - Add calendar. Click on it to add a new calednar</li>
+                        <li><strong>Button 2</strong> - Calendars help box.</li>
+                    </ul>
+
+                    <h6>Calendars list</h6>
+                    <ul>
+                        <li><strong>Item 3</strong> - Calendar ID.</li>
+                        <li><strong>Item 4</strong> - Calendar name.</li>
+                        <li><strong>Item 5</strong> - Calendar author.</li>
+                        <li><strong>Item 6</strong> - The number of pending reservations.</li>
+                        <li><strong>Item 7</strong> - The number of approved reservations.</li>
+                        <li><strong>Item 8</strong> - The number of rejected reservations.</li>
+                        <li><strong>Item 9</strong> - The number of canceled reservations.</li>
+                    </ul>  
+
+                    <p>
+                        <img src="http://envato-help.dotonpaper.net/booking-system-wordpress-plugin/content/back-end-calendars-description.jpg" alt="Booking System WordPress Plugin - Back end calendars" title="Booking System WordPress Plugin - Back end calendars" />
+                    </p>
+
+                    <h6>Calendar buttons</h6>
+                    <ul>
+                        <li><strong>Button 10</strong> - Display calendar to set availability.</li>
+                        <li><strong>Button 11</strong> - Display calendar settings.</li>
+                        <li><strong>Button 12</strong> - Display calendar notifications settings.</li>
+                        <li><strong>Button 13</strong> - Display calendar payment gateways settings.</li>
+                        <li><strong>Button 14</strong> - Display calendar user permissions settings.</li>
+                        <li><strong>Button 15</strong> - Delete calendar. All data related to this calendar will be deleted, including reservations.</li>
+                        <li><strong>Button 16</strong> - Calendar help box.</li>
+                    </ul> 
+
+                    <h6>Availability form</h6>
+                    <ul>
+                        <li><strong>Button 17</strong> - Submit form data. Click on it to set availability.</li>
+                        <li><strong>Button 18</strong> - Reset (remove) selected period data. Click on it to remove availability.</li>
+                        <li><strong>Button 19</strong> - Close form. Click on it to cancel the availability form.</li>
+                        <li><strong>Item 20</strong> - The first day of the period selected to set availability.</li>
+                        <li><strong>Item 21</strong> - The last day of the period selected to set availability.</li>
+                        <li><strong>Item 22</strong> - Set selected days/hours status.</li>
+                        <li><strong>Item 23</strong> - Set selected days/hours price.</li>
+                        <li><strong>Item 24</strong> - Set selected days/hours promotional price.</li>
+                        <li><strong>Item 25</strong> - Set selected days/hours number of items available. Leave it blank to not display any number. The default value is 1.</li>
+                        <li><strong>Item 26</strong> - Set selected days/hours information, which will be visible to the users.</li>
+                        <li><strong>Item 27</strong> - Set selected days/hours notes, which will be visible only to the administrators.</li>
+                        <li><strong>Item 28</strong> - Group selected days/hours into a single block. All data will be attributed to it.</li>
+                    </ul>
                 </section>
             </div>

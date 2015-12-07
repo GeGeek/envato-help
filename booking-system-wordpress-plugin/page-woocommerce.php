5d319be826ab0d3aa05cb824992b5f251013d900
Marius <marius.donea@dotonpaper.net>
2015-12-07 11:44:01 +0200

    v1.5.8

--- booking-system-wordpress-plugin/page-woocommerce.php
+++ booking-system-wordpress-plugin/page-woocommerce.php
@@ -2,7 +2,22 @@
             <div id="main">
                 <section id="getting-started">
                     <h2>How to use it with WooCommerce</h2>
-                    
+                </section>
+                
+                <section>
+                    <h4>Starting with version 2.1.2</h4>
+                    <p>
+                        This are beta instructions (very simple). We are trying to remove <strong>Step 2</strong>.
+                    </p>
+                    <ul>
+                        <li><strong>Step 1</strong> - Create a simple product.</li>
+                        <li><strong>Step 2</strong> - Add price to product.</li>
+                        <li><strong>Step 3</strong> - Attach a calendar in "Pinpoint Booking System" tab.</li>
+                    </ul>
+                </section>
+                
+                <section>
+                    <h4>Before version 2.1.2</h4>
                     <p>
                         <a href="http://envato-help.dotonpaper.net/booking-system-wordpress-plugin/content/woocommerce.png" target="_blank">
                             <img src="http://envato-help.dotonpaper.net/booking-system-wordpress-plugin/content/woocommerce.png" alt="" />
